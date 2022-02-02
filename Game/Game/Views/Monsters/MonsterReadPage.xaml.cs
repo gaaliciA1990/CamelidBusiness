7b619111fb08a3be396baa67f94365e95fc40923
khometit <averydinh1996@gmail.com>
2022-02-01 21:41:38 -0800

    Updated code to show UniqueItem slot in MonsterReadPage.xaml.cs

--- Game/Game/Views/Monsters/MonsterReadPage.xaml.cs
+++ Game/Game/Views/Monsters/MonsterReadPage.xaml.cs
@@ -5,6 +5,7 @@ using Xamarin.Forms.Xaml;
 
 using Game.ViewModels;
 using Game.Models;
+using System.Linq;
 
 namespace Game.Views
 {
@@ -32,6 +33,8 @@ namespace Game.Views
             InitializeComponent();
 
             BindingContext = this.ViewModel = data;
+
+            AddItemsToDisplay();
         }
 
         /// <summary>
@@ -65,5 +68,74 @@ namespace Game.Views
         {
             await Navigation.PushAsync(new NavigationPage(new MonsterIndexPage()));
         }
+
+        #region UniqueItemDisplay
+        /// <summary>
+        /// Show the Items the Character has
+        /// </summary>
+        public void AddItemsToDisplay()
+        {
+            var FlexList = ItemBox.Children.ToList();
+            foreach (var data in FlexList)
+            {
+                _ = ItemBox.Children.Remove(data);
+            }
+
+            //Add a StackLayout for each of the children 
+            //Placeholder, unique items will have their unique location enum
+            ItemBox.Children.Add(GetItemToDisplay(ItemLocationEnum.Head));
+        }
+
+        /// <summary>
+        /// Look up the Item to Display
+        /// </summary>
+        /// <param name="location"></param>
+        /// <returns></returns>
+        public StackLayout GetItemToDisplay(ItemLocationEnum location)
+        {
+            // Get the Item, if it exist show the info
+            // If it does not exist, show a Plus Icon for the location
+
+            // Defualt Image is the Plus
+            var ImageSource = "icon_add.png";
+
+            var data = ViewModel.Data.GetItemByLocation(location);
+            if (data == null)
+            {
+                data = new ItemModel { Location = location, ImageURI = ImageSource };
+            }
+
+            // Hookup the Image Button to show the Item picture
+            var ItemButton = new ImageButton
+            {
+                Style = (Style)Application.Current.Resources["ImageMediumStyle"],
+                Source = data.ImageURI
+            };
+
+            // Add the Display Text for the item
+            var ItemLabel = new Label
+            {
+                Text = location.ToMessage(),
+                Style = (Style)Application.Current.Resources["ValueStyleMicro"],
+                HorizontalOptions = LayoutOptions.Center,
+                HorizontalTextAlignment = TextAlignment.Center
+            };
+
+            // Put the Image Button and Text inside a layout
+            var ItemStack = new StackLayout
+            {
+                Padding = 3,
+                Style = (Style)Application.Current.Resources["ItemImageLabelBox"],
+                HorizontalOptions = LayoutOptions.Center,
+                Children = {
+                    ItemButton,
+                    ItemLabel
+                },
+            };
+
+            return ItemStack;
+        }
+
+        #endregion UniqueItemDisplay
     }
 }
