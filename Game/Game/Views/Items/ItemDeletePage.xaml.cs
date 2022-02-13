5f8e9649c43cd01bec01af68c777587ed69734a2
Carlo Provenzani <cprovenzani@seattleu.edu>
2022-02-13 15:37:49 -0800

    Changed dropdowns to have rounded corners and title for item CRUDi; ItemDeletePage.xaml, ItemDeletePage.xaml.cs

--- Game/Game/Views/Items/ItemDeletePage.xaml.cs
+++ Game/Game/Views/Items/ItemDeletePage.xaml.cs
@@ -28,11 +28,6 @@ namespace Game.Views
             InitializeComponent();
 
             BindingContext = this.viewModel = data;
-            LocationPicker.SelectedItem = data.Data.Location.ToString();
-            LocationPicker.IsEnabled = false;
-            AttributePicker.SelectedItem = data.Data.Attribute.ToString();
-            AttributePicker.IsEnabled = false;
-
 
             RangeBar.Progress = (float)viewModel.Data.Range / 15;
             ValueBar.Progress = (float)viewModel.Data.Value / 15;
