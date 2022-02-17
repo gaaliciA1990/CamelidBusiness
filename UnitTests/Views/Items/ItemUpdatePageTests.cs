5cb06df11193fe034fad31777fa3dbcdf9625572
Carlo Provenzani <cprovenzani@seattleu.edu>
2022-02-16 18:13:44 -0800

    Added ItemUpdatePage UTs, ItemUpdatePageTests.cs

--- UnitTests/Views/Items/ItemUpdatePageTests.cs
+++ UnitTests/Views/Items/ItemUpdatePageTests.cs
@@ -80,10 +80,11 @@ namespace UnitTests.Views
         }
 
         [Test]
-        public void ItemUpdatePage_Save_Clicked_Null_Image_Should_Pass()
+        public void ItemUpdatePage_Save_Clicked_Invalid_Invalid_Description_Should_Pass()
         {
             // Arrange
-            page.ViewModel.Data.ImageURI = null;
+            page.ViewModel.Data.Name = "";
+            page.ViewModel.Data.Description = "";
 
             // Act
             page.Save_Clicked(null, null);
@@ -93,6 +94,47 @@ namespace UnitTests.Views
             // Assert
             Assert.IsTrue(true); // Got to here, so it happened...
         }
+
+        [Test]
+        public void ItemUpdatePage_Save_Click_Valid_Location_And_Attribute_Should_Show_Pass()
+        {
+            // Arrange
+            page.ViewModel.Data.Name = "test";
+            page.ViewModel.Data.Description = "test";
+            page.errors.Add("ERROR","ERROR");
+            page.ViewModel.Data.ImageURI = null;
+
+            // Act
+            page.Save_Clicked(null, null);
+
+            //Reset
+
+            //Assert
+            Assert.IsTrue(true);
+        }
+
+        [Test]
+        public void ItemUpdatePage_Save_Click_Invalid_Location_And_Attribute_Should_Show_Pass()
+        {
+            // Arrange
+            page.ViewModel.Data.Name = "test";
+            page.ViewModel.Data.Description = "test";
+            page.ViewModel.Data.Location = ItemLocationEnum.Unknown;
+            page.ViewModel.Data.Attribute = AttributeEnum.Unknown;
+            page.ViewModel.Data.ImageURI = null;
+
+            // Act
+            page.Save_Clicked(null, null);
+
+            //Reset
+
+            //Assert
+            Assert.IsTrue(page.errors.ContainsKey("Location") == false);
+            Assert.IsTrue(page.errors.ContainsKey("Attribute") == false);
+        }
+
+
+
 
         [Test]
         public void ItemUpdatePage_OnBackButtonPressed_Valid_Should_Pass()
@@ -173,5 +215,108 @@ namespace UnitTests.Views
             // Assert
             Assert.IsTrue(true); // Got to here, so it happened...
         }
+
+
+
+        [Test]
+        public void ItemUpdatePage_LeftButton_Clicked_0_Should_Set_Image_At_Last_Index()
+        {
+            // Arrange
+            page.imageIndex = 0;
+
+            //Act
+            page.LeftButton_Clicked(null, null);
+
+            //Reset
+
+
+            //Assert
+            Assert.IsTrue(page.ViewModel.Data.ImageURI == page.imageList[page.imageList.Count - 1]);
+        }
+
+
+        [Test]
+        public void ItemUpdatePage_LeftButton_Clicked_1_Should_Set_Image_At_Index_0()
+        {
+            // Arrange
+            page.imageIndex = 1;
+
+            //Act
+            page.LeftButton_Clicked(null, null);
+
+            //Reset
+
+            // Assert
+            Assert.IsTrue(page.ViewModel.Data.ImageURI == page.imageList[0]);
+        }
+
+
+        [Test]
+        public void ItemUpdatePage_RightButton_Clicked_0_Should_Set_Image_At_Index_1()
+        {
+            // Arrange
+            page.imageIndex = 0;
+
+            // Act
+            page.RightButton_Clicked(null, null);
+
+            // Reset
+
+            //Assert
+            Assert.IsTrue(page.ViewModel.Data.ImageURI == page.imageList[1]);
+        }
+
+
+        [Test]
+        public void ItemUpdatePage_RightButton_Clicked_Last_Index_Should_Set_Image_at_index_0()
+        {
+            // Arrange
+            page.imageIndex = page.imageList.Count - 1;
+
+            //Act
+            page.RightButton_Clicked(null, null);
+
+            //Reset
+
+            //Assert
+            Assert.IsTrue(page.ViewModel.Data.ImageURI == page.imageList[0]);
+        }
+
+        [Test]
+        public void Update_AttributePicker_SelectedIndexChanged_Invalid_Location_And_Attribute_Should_Pass()
+        {
+            // Arrange
+            page.errors.Add("LocationAttribute", "ERROR");
+            page.ViewModel.Data.Attribute = AttributeEnum.Unknown;
+
+            // Act
+            page.AttributePicker_SelectedIndexChanged(null, null);
+
+            // Reset
+
+            // Assert
+            Assert.IsTrue(page.errors.ContainsKey("LocationAttribute") == true);
+        }
+
+
+        [Test]
+        public void ItemUpdatePage_LocationPicker_SelectedIndexChanged()
+        {
+            // Arrange
+            var LocationPicker = (Picker)page.FindByName("LocationPicker");
+            LocationPicker.SelectedIndex = 1;
+
+            // Act
+            page.LocationPicker_SelectedIndexChanged(LocationPicker, null);
+
+            // Reset
+
+            //Assert
+            Assert.IsTrue(page.ViewModel.Data.Location == ItemLocationEnum.Head);
+        }
+
+        
+
+
     }
 }
