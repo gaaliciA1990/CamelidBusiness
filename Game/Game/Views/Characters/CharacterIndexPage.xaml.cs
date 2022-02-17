e6b426872f1c38cb7767391e318cee58b95caffa
Alicia Garcia <garcia.alicia1990@gmail.com>
2022-02-16 22:50:28 -0800

    Made private method public to implement UT in CharacterIndexPage.xaml.cs

--- Game/Game/Views/Characters/CharacterIndexPage.xaml.cs
+++ Game/Game/Views/Characters/CharacterIndexPage.xaml.cs
@@ -93,7 +93,7 @@ namespace Game.Views
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
-        private async void Back_Clicked(object sender, EventArgs e)
+        public async void Back_Clicked(object sender, EventArgs e)
         {
             await Navigation.PushAsync(new NavigationPage(new VillagePage()));
         }
