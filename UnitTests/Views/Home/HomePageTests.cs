1076acde809b2c713e0d008961594b2dcb0c53d5
Carlo Provenzani <cprovenzani@seattleu.edu>
2022-02-16 20:05:32 -0800

    Added HomePage UTs; HomePageTests.cs

--- UnitTests/Views/Home/HomePageTests.cs
+++ UnitTests/Views/Home/HomePageTests.cs
@@ -58,5 +58,19 @@ namespace UnitTests.Views
             // Assert
             Assert.IsTrue(true); // Got to here, so it happened...
         }
+
+        [Test]
+        public void HomePage_StartButton_Clicked_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            page.AboutButton_Clicked(null, null);
+
+            // Reset
+
+            // Assert
+            Assert.IsTrue(true); // Got to here, so it happened...
+        }
     }
 }
