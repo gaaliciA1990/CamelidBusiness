e8df8beb46b110049139dd1a66836c33efeb72f8
Alicia Garcia <garcia.alicia1990@gmail.com>
2022-02-16 23:36:29 -0800

    Made Back_clicked method public to implement UT in CharacterReadPage.xaml.cs

--- Game/Game/Views/Characters/CharacterReadPage.xaml.cs
+++ Game/Game/Views/Characters/CharacterReadPage.xaml.cs
@@ -188,7 +188,7 @@ namespace Game.Views
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
-        private async void Back_Clicked(object sender, EventArgs e)
+        public async void Back_Clicked(object sender, EventArgs e)
         {
             await Navigation.PushAsync(new NavigationPage(new CharacterIndexPage()));
         }
