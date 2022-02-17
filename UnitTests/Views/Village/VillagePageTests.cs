21784d5ad930ed5ad2c954b65c9c6e7b46ec287a
Carlo Provenzani <cprovenzani@seattleu.edu>
2022-02-16 20:10:17 -0800

    Added VillagePage UTs; VillagePageTests.cs

--- UnitTests/Views/Village/VillagePageTests.cs
+++ UnitTests/Views/Village/VillagePageTests.cs
@@ -100,5 +100,18 @@ namespace UnitTests.Views
             // Assert
             Assert.IsTrue(true); // Got to here, so it happened...
         }
+
+        [Test]
+        public void VillagePage_HomeButton_Clicked_Default_Should_Pass()
+        {
+            // Arrange
+            // Act
+            page.HomeButton_Clicked(null, null);
+
+            // Reset
+
+            // Assert
+            Assert.IsTrue(true); // Got to here, so it happened...
+        }
     }
 }
