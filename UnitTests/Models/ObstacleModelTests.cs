7b6d6a5385c2f1a05461e538706e8c5bd6c90d27
Carlo Provenzani <cprovenzani@seattleu.edu>
2022-03-03 17:39:44 -0800

    Added UTs for ObstacleModel; ObstacleModelTests.cs

--- UnitTests/Models/ObstacleModelTests.cs
+++ UnitTests/Models/ObstacleModelTests.cs
@@ -30,6 +30,58 @@ namespace UnitTests.Models
             Assert.IsNotNull(result);
         }
 
-      
+        [Test]
+        public void ObstacleModel_Constructor_Default_With_PbstacleModel_Parameter_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = new ObstacleModel(new ObstacleModel());
+
+            // Reset
+
+            // Assert 
+            Assert.IsNotNull(result);
+        }
+
+
+        [Test]
+        public void ObstacleModel_Update_Default_Should_Pass()
+        {
+            // Arrange
+            var dataOriginal = new ObstacleModel();
+            dataOriginal.CurrentHealth = 10;
+            dataOriginal.MaxHealth = 10;
+
+            var dataNew = new ObstacleModel();
+            dataNew.CurrentHealth = 20;
+            dataNew.MaxHealth = 20;
+
+            // Act
+            var result = dataOriginal.Update(dataNew);
+
+            // Reset
+
+            // Assert 
+            Assert.AreEqual(20, dataOriginal.CurrentHealth);
+            Assert.AreEqual(20, dataOriginal.MaxHealth);
+        }
+
+
+        [Test]
+        public void ObstacleModel_Update_Null_Should_Fail()
+        {
+            // Arrange
+            var dataOriginal = new ObstacleModel();
+
+            // Act
+            var result = dataOriginal.Update(null);
+
+            // Reset
+
+            // Assert 
+            Assert.AreEqual(false, result);
+        }
+
     }
 }
