5048bb0e0602a72743ce43dd16e33c2f037dcd91
Carlo Provenzani <cprovenzani@seattleu.edu>
2022-03-03 17:27:42 -0800

    Added ObstacleModelTests.cs

---
+++ UnitTests/Models/ObstacleModelTests.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+
+using Game.Models;
+using Game.ViewModels;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UnitTests.Models
+{
+    [TestFixture]
+    public class ObstacleModelTests
+    {
+        [TearDown]
+        public void TearDown()
+        {
+            ItemIndexViewModel.Instance.Dataset.Clear();
+        }
+
+        [Test]
+        public void ObstacleModel_Constructor_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = new ObstacleModel();
+
+            // Reset
+
+            // Assert 
+            Assert.IsNotNull(result);
+        }
+
+      
+    }
+}
